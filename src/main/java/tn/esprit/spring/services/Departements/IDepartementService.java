f23361f2880f318d2f4cf777743b1edcecd3a415
TarekBenYahia <Tarekbenyahia89@gmail.com>
2021-10-24 15:23:25 +0100

    Interfaces

--- src/main/java/tn/esprit/spring/services/Departements/IDepartementService.java
+++ src/main/java/tn/esprit/spring/services/Departements/IDepartementService.java
@@ -1,4 +1,14 @@
 package tn.esprit.spring.services.Departements;
 
+import tn.esprit.spring.entities.Departement;
+
+import java.util.List;
+
 public interface IDepartementService {
+
+    List<Departement> retrieveAllDepartements();
+    Departement addDepartement(Departement e);
+    void deleteDepartement(String id);
+    Departement updateDepartement(Departement e);
+    Departement retrieveDepartement(String id);
 }
